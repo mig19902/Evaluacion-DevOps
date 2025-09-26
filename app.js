2fabf6bfa316e05b2c3e48b2b277d93ef3c399a5
MigJes0001 <mj423627@gmail.com>
2025-09-26 19:19:24 +0200

    Quinto intento modificar ruta raíz y puerto

--- app.js
+++ app.js
@@ -5,21 +5,8 @@ const app = express();
 // Con esta declaración podemos usar rutas absolutas con compatibilidad para cualquier sistema operativo
 const path = require('path');
 
-
-  //// El puerto que más tarde modificaremos // 
-const port = process.env.PORT || 3000;  
-
-// Middleware para servir archivos estáticos en este caso HTML desde la carpeta public
-app.use(express.static('public')); // Ya de por sí sirve los archivos estáticos en la carpeta public
-
-
-/// PRIMERA PETCIÓN GET ENTREGA CON RES.SENDFILE UN ARCHIVO ESTÁTICO HTML --
-//  DIRNAME ESTABLECE LA RUTA ABSOLUTA DEL DIRECTORIO A index1.html
-
-app.get('/index1', (req, res) => {
-  res.sendFile(__dirname + '/public/index1.html');
-});
-
+   
+const port = process.env.PORT || 3000;
 
 // SEGUNDA PETICIÓN GET ENTREGA CON RES.SENDFILE UN ARCHIVO ESTÁTICO HTML --
 //  DIRNAME ESTABLECE LA RUTA ABSOLUTA DEL DIRECTORIO A index2.html
